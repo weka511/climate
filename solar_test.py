c5d076122ef3a893252c6faa230130b1246a0703
Simon Crase <simon@greenweaves.nz>
2017-04-06 13:57:47 +1200

    Plotting delination and length of day

--- solar_test.py
+++ solar_test.py
@@ -67,51 +67,59 @@ def lat(i):
         return '{0}N'.format(i)
     return '0'
 
-def declination(day): #Jan 1 is day zero
+def true_longitude(day):
     M=2*m.pi*(day-2)/365 #perihelion Jan 3
     E=k.get_eccentric_anomaly(M,0.017)
     true_anomaly=k.get_true_anomaly(E,0.017)
-    true_longitude = k.true_longitude_from_true_anomaly(true_anomaly)
-    return m.asin(s.sin_declination(m.radians(23.4),true_longitude))
+    return k.true_longitude_from_true_anomaly(true_anomaly)
+    
+def declination(day): #Jan 1 is day zero
+    return m.asin(s.sin_declination(m.radians(23.4),true_longitude(day)))
 
- 
+days=list(itertools.accumulate([31,28,31,30,31,30,31,31,30,31,30,31])) 
 earth = Earth()
 solar = s.Solar(earth)
+
 fig, ax = plt.subplots()
+plt.figure(1,figsize=(20,20))
+ax1=plt.subplot(221)
+
 xs=list(range(0,366))
 ys=[m.degrees(declination(day)) for day in xs]
 plt.plot(xs,ys)
 plt.title('Solar declination of Earth')
 plt.ylabel('Declination - degrees')
-ax.set_xticks([i for i in itertools.accumulate([31,28,31,30,31,30,31,31,30,31,30,31])])
-ax.set_xticklabels(['JFMAMJJASOND'[i] for i in range(0,12)])        
+ax1.set_xticks([i for i in days])
+ax1.set_xticklabels(['JFMAMJJASOND'[i] for i in range(0,12)])        
+plt.savefig('declination.png')
 
- 
+ax2=plt.subplot(222)
 
- 
- #def day_length(true_longitude,latitude):
-     #ha1=solar.ha_sunrise_sunset(math.radians(true_longitude),math.radians(latitude),sunset=False)
-     #ha2=solar.ha_sunrise_sunset(math.radians(true_longitude),math.radians(latitude))
-     #print ('{0:7.2f}, {1:7.2f}, {2:7.2f}'.format(true_longitude,latitude,math.degrees(ha2-ha1)/15))
-     #return math.degrees(ha2-ha1)/15
- 
- #x = np.linspace(-90, 270,num=8) 
- #y = np.linspace(-90,  90,num=10) 
+def day_length(day,latitude):
+    phi=true_longitude(day)
+    ha1=solar.ha_sunrise_sunset(phi,m.radians(latitude),sunset=False)
+    ha2=solar.ha_sunrise_sunset(phi,m.radians(latitude))
+    print ('{0:7.2f}, {1:7.2f}, {2:7.2f}'.format(phi,latitude,m.degrees(ha2-ha1)/15))
+    return m.degrees(ha2-ha1)/15
 
- #X, Y = np.meshgrid(x, y) 
- #Z = (np.vectorize(day_length))(X,Y) 
- #fig, ax = plt.subplots()
- #cax=plt.pcolormesh(X, Y, Z, cmap = cm.jet) 
- #cbar = fig.colorbar(cax)
- #plt.xlim(-90,271)
- #plt.ylim([-90,90])
- #ax.set_xticks([i for i in range(-90,271,30)])
- #ax.set_xticklabels(['JFMAMJJASOND'[i] for i in range(0,12)])    
- #ax.set_yticks([i for i in range(-90,91,30)])
- #ax.set_yticklabels([lat(i) for i in range(-90,91,30)])
- #plt.title('Length of Day')
- #plt.savefig('langth-of-day.png')
- #plt.show()     
+x = np.linspace(0, 366) 
+y = np.linspace(-90,  90) 
+
+X, Y = np.meshgrid(x, y) 
+Z = (np.vectorize(day_length))(X,Y) 
+
+cax=plt.pcolormesh(X, Y, Z, cmap = cm.jet) 
+cbar = fig.colorbar(cax)
+#plt.xlim(-90,271)
+#plt.ylim([-90,90])
+ax2.set_xticks([i for i in days])
+ax2.set_xticklabels(['JFMAMJJASOND'[i] for i in range(0,12)])    
+ax2.set_yticks([i for i in range(-90,90,30)])
+ax2.set_yticklabels([lat(i) for i in range(-90,91,30)])
+plt.title('Length of Day')
+plt.savefig('length-of-day.png')
+#plt.show()     
+ 
  
  #for latitude in range(-90,90,10):
      #for longitude in range(-90,271,12):
