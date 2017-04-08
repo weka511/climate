92065df7251353ba86895e540b6794ed7ac3868d
Simon Crase <simon@greenweaves.nz>
2017-04-08 15:13:53 +1200

    Added new function for daialy irradiance

--- solar_test.py
+++ solar_test.py
@@ -33,7 +33,7 @@ class Earth:
     def instantaneous_distance(self,true_longitude):
         '''
         Instantaneaous Distance from Sun in AU
-        Appelbaum & Flood equations (2) & (3)
+        
         Parameters:
              true_longitude
         '''
@@ -43,7 +43,7 @@ class Earth:
     def sin_declination(self,true_longitude):
         '''
         Sine of declination
-        Appelbaum & Flood equation (7)
+        
         Parameters:
              true_longitude        
         '''
@@ -52,9 +52,6 @@ class Earth:
     def cos_zenith_angle(self,true_longitude,latitude,T):
         '''
         Cosine of zenith angle
-        Appelbaum & Flood equation (6)
-        See also Derivation of the solar geometric 
-        relationships using vector analysis by Alistair Sproul
 
         Renewable Energy 32 (2007) 1187-1205
         '''
@@ -76,7 +73,7 @@ def true_longitude(day):
 def declination(day): #Jan 1 is day zero
     return m.asin(s.sin_declination(m.radians(23.4),true_longitude(day)))
 
-def days():
+def lengths_of_months():
     return list(itertools.accumulate([31,28,31,30,31,30,31,31,30,31,30,31]))
 
 earth = Earth()
@@ -86,26 +83,26 @@ fig, ax = plt.subplots()
 plt.figure(1,figsize=(40,40))
 ax1=plt.subplot(221)
 
+# Plot Solar declination of Earth
+
 xs=list(range(0,366))
 ys=[m.degrees(declination(day)) for day in xs]
 plt.plot(xs,ys)
 plt.title('Solar declination of Earth')
 plt.ylabel('Declination - degrees')
-ax1.set_xticks([i for i in days()])
+ax1.set_xticks([i for i in lengths_of_months()])
 ax1.set_xticklabels(['JFMAMJJASOND'[i] for i in range(0,12)])        
-plt.savefig('declination.png')
+
+
+# Plot Length of Day
 
 def day_length(day,latitude):
-    true_long=true_longitude(day)
-    ha1=solar.ha_sunrise_sunset(true_long,m.radians(latitude),sunset=False)
-    ha2=solar.ha_sunrise_sunset(true_long,m.radians(latitude))
-    #print ('{0:7.2f}, {1:7.2f}, {2:7.2f}'.format(true_long,latitude,m.degrees(ha2-ha1)/15))
-    return m.degrees(ha2-ha1)/15
+    return solar.length_of_day(true_longitude(day),m.radians(latitude))
 
 ax2=plt.subplot(222)
 
 x = np.linspace(0, 366) 
-y = np.linspace(-90,  90) 
+y = np.linspace(-90,  91) 
 
 X, Y = np.meshgrid(x, y) 
 Z = (np.vectorize(day_length))(X,Y) 
@@ -114,17 +111,22 @@ cax=plt.pcolormesh(X, Y, Z, cmap = cm.jet)
 cbar = fig.colorbar(cax)
 #plt.xlim(-90,271)
 #plt.ylim([-90,90])
-ax2.set_xticks([i for i in days()])
+ax2.set_xticks([i for i in lengths_of_months()])
 ax2.set_xticklabels(['JFMAMJJASOND'[i] for i in range(0,12)])    
-ax2.set_yticks([i for i in range(-90,90,30)])
+ax2.set_yticks([i for i in range(-90,91,30)])
 ax2.set_yticklabels([lat(i) for i in range(-90,91,30)])
 plt.title('Length of Day')
-plt.savefig('length-of-day.png')
 
 
-###
+# Plot zenith angle at noon
+
 def za_noon(day,latitude):
-    return m.degrees(m.acos(s.cos_zenith_angle(m.radians(23.4),true_longitude(day),latitude,0)))
+    return m.degrees(
+        m.acos(
+            s.cos_zenith_angle(m.radians(23.4),
+                               true_longitude(day),
+                               m.radians(latitude),
+                               12)))
 
 ax3=plt.subplot(223)
 
@@ -138,39 +140,42 @@ cax=plt.pcolormesh(X, Y, Z, cmap = cm.jet)
 cbar = fig.colorbar(cax)
 #plt.xlim(-90,271)
 #plt.ylim([-90,90])
-ax3.set_xticks([i for i in days()])
+ax3.set_xticks([i for i in lengths_of_months()])
 ax3.set_xticklabels(['JFMAMJJASOND'[i] for i in range(0,12)])    
 ax3.set_yticks([i for i in range(-90,90,30)])
 ax3.set_yticklabels([lat(i) for i in range(-90,91,30)])
 plt.title('Zenith Angle Noon')
-plt.savefig('za-noon.png')
-###
 
-
-#plt.show()     
+     
  
  
  #for latitude in range(-90,90,10):
      #for longitude in range(-90,271,12):
          #print (longitude,latitude,solar.ha_sunrise_sunset(longitude,latitude))
          
- #def surface_irradience(true_longitude,latitude):
-     #return sum([solar.surface_irradience(math.radians(true_longitude),math.radians(latitude),T) for T in range(0,23)]) 
+def surface_irradience(true_longitude,latitude):
+    #ha=s.hour_angle_sunrise_sunset(m.radians(true_longitude),m.radians(latitude))
+    #return sum([solar.surface_irradience(m.radians(true_longitude),m.radians(latitude),T) for T in range(0,23)]) 
+    return solar.surface_irradience_daily(m.radians(true_longitude),m.radians(latitude))
 
- #x = np.linspace(-90, 270,num=360) 
- #y = np.linspace(-90,90,num=180) 
+ax4=plt.subplot(224)
 
- #X, Y = np.meshgrid(x, y) 
- #Z = (np.vectorize(surface_irradience))(X,Y) 
- #fig, ax = plt.subplots()
- #cax=plt.pcolormesh(X, Y, Z, cmap = cm.jet) 
- #cbar = fig.colorbar(cax)
- #plt.xlim(-90,271)
- #plt.ylim([-90,90])
- #ax.set_xticks([i for i in range(-90,271,30)])
- #ax.set_xticklabels(['JFMAMJJASOND'[i] for i in range(0,12)])    
- #ax.set_yticks([i for i in range(-90,91,30)])
- #ax.set_yticklabels([lat(i) for i in range(-90,91,30)])
- #plt.title('Surface Irradiance')
- #plt.savefig('surface.png')
- #plt.show() 
+x = np.linspace(-90, 270,num=360) 
+y = np.linspace(-90,90,num=180) 
+
+X, Y = np.meshgrid(x, y) 
+Z = (np.vectorize(surface_irradience))(X,Y) 
+#fig, ax = plt.subplots()
+cax=plt.pcolormesh(X, Y, Z, cmap = cm.jet) 
+cbar = fig.colorbar(cax)
+plt.xlim(-90,271)
+plt.ylim([-90,90])
+ax4.set_xticks([i for i in range(-90,271,30)])
+ax4.set_xticklabels(['JFMAMJJASOND'[i] for i in range(0,12)])    
+ax4.set_yticks([i for i in range(-90,91,30)])
+ax4.set_yticklabels([lat(i) for i in range(-90,91,30)])
+plt.title('Surface Irradiance')
+
+plt.savefig('solar-tests.png')
+
+plt.show() 
